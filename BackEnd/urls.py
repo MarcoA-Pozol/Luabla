de20a34bafd9fb50ae20e33478e29007bcf6dd38
MarcoA-Pozol <marcoantoniopozolnarciso@gmail.com>
2024-08-15 13:22:36 -0600

    Implemented API endpoints to delete, patch and update user, and decks and cards list retrieving endpoints. Version 0.0.4

--- BackEnd/urls.py
+++ BackEnd/urls.py
@@ -1,14 +1,18 @@
 from django.urls import path
-from .views import ListUsersView, CreateUserView, CreateDeckView, CreateCardView
+from .views import ListUsersView, CreateUserView, UpdateUserView, DeleteUserView, ListDecksView, CreateDeckView, ListCardsView, CreateCardView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     # User API Views
     path("user/list/", ListUsersView.as_view(), name="api-list-users"),
     path("user/create/", CreateUserView.as_view(), name="api-create-user"),
+    path("user/update/<int:pk>", UpdateUserView.as_view(), name="api-update-user"),
+    path("user/delete/<int:pk>", DeleteUserView.as_view(), name="api-delete-user"),
     # Deck(App_Cards) API Views
+    path("deck/list/", ListDecksView.as_view(), name="api-list-decks"),
     path("deck/create/", CreateDeckView.as_view(), name="api-create-deck"),
     # Card(App_Cards) API Views
+    path("card/list/", ListCardsView.as_view(), name="api-list-cards"),
     path("card/create/", CreateCardView.as_view(), name="api-create-card"),
     # JWT management urls for the Token
     path("token/get/", TokenObtainPairView.as_view(), name="token-obtain"),
--- BackEnd/views.py
+++ BackEnd/views.py
@@ -24,7 +24,7 @@ class ListUsersView(APIView):
         """
             Retrieve list of users without returning "password" for security as a second filter, the first is the JWT authorization required.
         """
-        users = User.objects.all().values('username', 'country', 'email', 'gender', 'learning_goals', 'profile_picture')
+        users = User.objects.all().values('id', 'username', 'country', 'email', 'gender', 'learning_goals', 'profile_picture')
         return Response(users, status = status.HTTP_200_OK)
     
 class CreateUserView(generics.CreateAPIView):
@@ -33,14 +33,53 @@ class CreateUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
     queryset = User.objects.all()
     
+class UpdateUserView(generics.UpdateAPIView):
+    permission_classes = [IsAuthenticated, IsAdminUser]
+    
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    
+class DeleteUserView(generics.DestroyAPIView):
+    permission_classes = [IsAuthenticated, IsAdminUser]
+    
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    
 # Deck API Views
+class ListDecksView(APIView):
+    """
+        API View that can retrieve a list of all decks from App_Cards Deck model.
+    """
+    permission_classes = [IsAuthenticated,]
+    
+    def get(self, request):
+        """
+            Retrieve list of decks
+        """
+        decks = Deck.objecst.all().values('id', 'title', 'author', 'description')
+        return Response(decks, status = status.HTTP_200_OK)
+
 class CreateDeckView(generics.CreateAPIView):
     permission_classes = [IsAuthenticated,]
     
     serializer_class = CreateDeckSerializer
     queryset = Deck.objects.all()
 
+
 # Card API Views
+class ListCardsView(APIView):
+    """
+        API View that can retrieve a list of all cards from App_Cards Card model.
+    """
+    permission_classes = [IsAuthenticated,]
+    
+    def get(self, request):
+        """
+            Retrieve a list of cards
+        """
+        cards = Card.objects.all().values('id', 'title', 'content', 'author', 'group')
+        return Response(cards, status = status.HTTP_200_OK)
+        
 class CreateCardView(generics.CreateAPIView):
     permission_classes = [IsAuthenticated,]
     
